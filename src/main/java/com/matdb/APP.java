deb69b6af0ac702c5256624ad8fc2122238e2904
lbs <2322701154@qq.com>
2023-12-11 16:07:32 +0800

    修复提交错误

--- src/main/java/com/matdb/APP.java
+++ src/main/java/com/matdb/APP.java
@@ -1,10 +1,13 @@
 package com.matdb;
 
+import com.matdb.config.CorsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 
 @SpringBootApplication
+@Import(CorsConfig.class)
 public class APP {
     public static void main(String[] args) {
         SpringApplication.run(APP.class,args);
