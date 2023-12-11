f47b7a387d2615121f3b93a1b0032a3f9f8df6c2
lbs <2322701154@qq.com>
2023-12-11 15:44:50 +0800

    前后端分离

--- src/main/java/com/matdb/controller/IndexController.java
+++ src/main/java/com/matdb/controller/IndexController.java
@@ -4,7 +4,6 @@ import com.matdb.domain.entity.UserEntity;
 import com.matdb.service.IndexService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,44 +16,24 @@ public class IndexController {
     @Autowired
     IndexService indexService;
 
-    @GetMapping("/")
-    public String login(){
-        System.out.println("\nlogin");
-        return "login";
-    }
-
     @GetMapping("/admin")
-    public String admin(@RequestParam("username") String username,@RequestParam("key") String key, Model model){
+    public String admin(@RequestParam("username") String username, @RequestParam("key") String key){
         UserEntity user = indexService.findUserByUsername(username);
         String password = user.getPassword();
         String userKey = user.getKey();
         if (Objects.equals(key, userKey)) {
-            model.addAttribute("username", username);
-            model.addAttribute("password",password);
-            model.addAttribute("key",key);
             System.out.println("\nadmin: username:" + username + "/password:" + password + "/key:" + key);
-            return "admin";
+            return "redirect:http://127.0.0.1:8083/admin?username=" + username + "&key=" + key;
         }
         return null;
     }
 
-
-    @GetMapping("/register")
-    public String register(){
-        System.out.println("\nregister");
-        return "register";
-    }
-
     @GetMapping("/index")
-    public String index(@RequestParam("uid") String uid, @RequestParam("username") String username, Model model){
+    public String index(@RequestParam("uid") String uid, @RequestParam("username") String username){
         UserEntity user = indexService.findUserByUsername(username);
         String password = user.getPassword();
         String key = user.getKey();
-        model.addAttribute("uid",uid);
-        model.addAttribute("username",username);
-        model.addAttribute("password",password);
-        model.addAttribute("key",key);
         System.out.println("\nindex: uid:" + uid + "/username:" + username + "/password:" + password + "/key:" + key);
-        return "index";
+        return "redirect:http://127.0.0.1:8083/index?uid=" + uid + "&username=" + username;
     }
 }
