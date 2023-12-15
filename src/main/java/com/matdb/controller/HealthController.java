b33a6becd7c45dd6e24457764d3d83e0a2a058ad
lbs <2322701154@qq.com>
2023-12-15 20:11:24 +0800

    完善健康检查

---
+++ src/main/java/com/matdb/controller/HealthController.java
@@ -0,0 +1,14 @@
+package com.matdb.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public void healthCheck(){
+        System.out.println("\nHealth checking.");
+    }
+
+}
